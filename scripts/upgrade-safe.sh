fd970c18f40105e3be1df56b6ccd9ae6e23bd062
Francisco Giordano <frangio.1@gmail.com>
2020-05-11 20:34:21 -0300

    replace search-and-replace with patches

--- scripts/upgrade-safe.sh
+++ scripts/upgrade-safe.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-git checkout contracts
+git checkout contracts test
 rm -rf build contracts/Initializable.sol
 
 npm run compile
@@ -15,15 +15,11 @@ mv __upgradeable__ contracts
 
 git checkout $(git ls-files -d)
 
-sed -i -e 's/(ERC1820Implementer/\0Upgradeable/' contracts/mocks/Create2Impl.sol
+# git add contracts
 
-rs "contract\.fromArtifact\('(.*?Mock.*?)'\)" "contract.fromArtifact('\${1}Upgradeable')"
+rs "contract\.fromArtifact\('([^I].*?)'\)" "contract.fromArtifact('\${1}Upgradeable')" test
 
-rs -w 'ContextMock context' 'ContextMockUpgradeable context' contracts/mocks/ContextMock.sol
-rs -F 'GSNRecipient._msg' 'GSNRecipientUpgradeable._msg' contracts/mocks/{,ERC721}GSNRecipientMock.sol
-rs '(new )Escrow|Escrow( private)' '${1}EscrowUpgradeable${2}' contracts/payment/PullPayment.sol
-rs -w 'ReentrancyAttack attacker' 'ReentrancyAttackUpgradeable attacker' contracts/mocks/ReentrancyMock.sol
-rs -F 'ERC20ReturnTrueMock(' 'ERC20ReturnTrueMockUpgradeable(' contracts/mocks/SafeERC20Helper.sol
+cat patches/* | patch -p1
 
 rs ' +$' '' contracts
 
