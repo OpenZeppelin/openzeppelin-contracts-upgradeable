58ac3758b4bfe3937bee4ef535ae24d67a60cad8
Francisco Giordano <frangio.1@gmail.com>
2020-11-06 18:52:00 -0300

    Add patch-upstream GitHub Actions Workflow

---
+++ scripts/merge-upstream.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail -x
+
+git fetch 'https://github.com/frangio/openzeppelin-contracts.git'
+git config user.name 'github-actions'
+git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
+git merge --no-edit FETCH_HEAD
+git push
