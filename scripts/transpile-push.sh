224c8fcc9948d364426aa7437ecd12c972e42ed9
Francisco Giordano <frangio.1@gmail.com>
2020-11-06 21:13:56 -0300

    improve transpile-push.sh script

--- scripts/transpile-push.sh
+++ scripts/transpile-push.sh
@@ -4,13 +4,12 @@ set -euo pipefail -x
 
 bash scripts/transpile.sh
 
-COMMIT="$(git rev-parse --short HEAD)"
-
 git config user.name 'github-actions'
 git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
 
-git add contracts
-git reset --soft master
-git checkout master
+COMMIT="$(git rev-parse --short HEAD)"
+
+git checkout --orphan master
+git reset --soft origin/master
 git commit -m "Transpile $COMMIT"
 git push origin master
