48ba1aba0fa6c8a930994713a9cc6a669348068d
Francisco Giordano <frangio.1@gmail.com>
2020-11-09 16:56:19 -0300

    eddit merge-upstream commit message

--- scripts/merge-upstream.sh
+++ scripts/merge-upstream.sh
@@ -5,5 +5,5 @@ set -euo pipefail -x
 git fetch 'https://github.com/OpenZeppelin/openzeppelin-contracts.git'
 git config user.name 'github-actions'
 git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
-git merge --no-edit FETCH_HEAD
+git merge FETCH_HEAD -m 'Merge upstream openzeppelin-contracts into upstream-patched'
 git push
