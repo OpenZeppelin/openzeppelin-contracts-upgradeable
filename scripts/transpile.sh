aa8c1582369210e4198af014b1f6b0169299869f
Francisco Giordano <frangio.1@gmail.com>
2020-11-11 00:20:51 -0300

    configure transpiler to emit public initializers for presets

--- scripts/transpile.sh
+++ scripts/transpile.sh
@@ -7,4 +7,8 @@ npm run compile
 # -D: delete original and excluded files
 # -i: use included Initializable
 # -x: exclude all proxy contracts
-npx @openzeppelin/upgrade-safe-transpiler -D -i contracts/proxy/Initializable.sol -x 'contracts/proxy/**/*'
+# -p: emit public initializer
+npx @openzeppelin/upgrade-safe-transpiler -D \
+  -i contracts/proxy/Initializable.sol \
+  -x 'contracts/proxy/**/*' \
+  -p 'contracts/presets/**/*'
