2423440d461c243301b92f6815c220139503d96f
Francisco Giordano <frangio.1@gmail.com>
2020-11-10 23:40:10 -0300

    configure transpiler to use built in Initializable and exclude all proxies

--- scripts/transpile.sh
+++ scripts/transpile.sh
@@ -4,4 +4,7 @@ set -euo pipefail -x
 
 npm run compile
 
-npx @openzeppelin/upgrade-safe-transpiler -D
+# -D: delete original and excluded files
+# -i: use included Initializable
+# -x: exclude all proxy contracts
+npx @openzeppelin/upgrade-safe-transpiler -D -i contracts/proxy/Initializable.sol -x 'contracts/proxy/**/*'
