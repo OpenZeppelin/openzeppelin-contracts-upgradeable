97277e4cf3cc1c419da4e3df6d357d14f5381336
Francisco Giordano <frangio.1@gmail.com>
2020-11-11 11:43:26 -0300

    repurpose transpile-push script to transpile-branch

---
+++ scripts/upgrade-safe/transpile-branch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+  echo "usage: bash $0 <branch> [base]" >&2
+  exit 1
+fi
+
+set -x
+
+branch="$1"
+base="${2:-}"
+
+bash scripts/upgrade-safe/transpile.sh
+bash scripts/upgrade-safe/git-user-config.sh
+
+commit="$(git rev-parse --short HEAD)"
+
+git add contracts
+git checkout --orphan "$branch"
+
+if [ -n "$base" ]; then
+  git reset --soft "$base"
+fi
+
+if ! git diff --cached --quiet; then
+  git commit -m "Transpile $commit"
+fi
