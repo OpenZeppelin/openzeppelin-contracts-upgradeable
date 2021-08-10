3d2feb037e1afe9435c94fa6eaea5edca1269a05
Francisco Giordano <frangio.1@gmail.com>
2021-08-10 16:00:38 -0300

    Transpile ba1ef2d3

--- scripts/upgradeable/merge-upstream.sh
+++ scripts/upgradeable/merge-upstream.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-set -euo pipefail -x
+set -euo pipefail
 
-git fetch 'https://github.com/OpenZeppelin/openzeppelin-contracts.git'
-git merge FETCH_HEAD -m 'Merge upstream openzeppelin-contracts into upstream-patched'
+: "${REF:="$(git rev-parse --symbolic-full-name HEAD)"}"
+
+if [[ "$REF" != refs/heads/* ]]; then
+  echo "$REF is not a branch" >&2
+  exit 1
+elif [[ "$REF" == refs/heads/patches ]]; then
+  echo "Run on a different branch" >&2
+  exit 1
+fi
+
+set -x
+
+input="${REF#refs/heads/}"
+upstream="${input#patched/}"
+branch="patched/$upstream"
+
+git checkout "$branch" 2>/dev/null || git checkout -b "$branch" patches
+git fetch 'https://github.com/OpenZeppelin/openzeppelin-contracts.git' "$upstream"
+git merge patches FETCH_HEAD -m "Merge upstream $upstream into $branch"
