b5f0d99ef38d282f8c33493efd5caa2925a2e5c0
Francisco Giordano <frangio.1@gmail.com>
2022-09-23 20:12:55 -0300

    filter out unreachable build-info files

--- scripts/upgradeable/transpile.sh
+++ scripts/upgradeable/transpile.sh
@@ -4,11 +4,20 @@ set -euo pipefail -x
 
 npm run compile
 
+build_info=($(jq -r '.input.sources | keys | if any(test("^contracts/mocks/.*\\\\bunreachable\\\\b")) then empty else input_filename end' artifacts/build-info/*))
+build_info_num=${#build_info[@]}
+
+if [ $build_info_num -ne 1 ]; then
+  echo "found $build_info_num relevant build info files but expected just 1"
+fi
+
 # -D: delete original and excluded files
+# -b: use this build info file
 # -i: use included Initializable
 # -x: exclude all proxy contracts except Clones library
 # -p: emit public initializer
 npx @openzeppelin/upgrade-safe-transpiler@latest -D \
+  -b "$build_info" \
   -i contracts/proxy/utils/Initializable.sol \
   -x 'contracts/proxy/**/*' \
   -x '!contracts/proxy/Clones.sol' \
