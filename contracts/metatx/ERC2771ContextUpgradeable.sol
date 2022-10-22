306a4c938584eb6afde74ef44cbb7c3b4a79160f
eliashezron <eliashezron23@gmail.com>
2022-10-22 23:23:36 +0000

    created intializer function in place for constructor

--- contracts/metatx/ERC2771ContextUpgradeable.sol
+++ contracts/metatx/ERC2771ContextUpgradeable.sol
@@ -9,20 +9,38 @@ import "../proxy/utils/Initializable.sol";
 /**
  * @dev Context variant with ERC2771 support.
  */
-abstract contract ERC2771ContextUpgradeable is Initializable, ContextUpgradeable {
-    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
-    address private immutable _trustedForwarder;
+abstract contract ERC2771ContextUpgradeable is
+    Initializable,
+    ContextUpgradeable
+{
+    address private _trustedForwarder;
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor(address trustedForwarder) {
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    function __ERC2771ContextUpgradeable_init(address trustedForwarder)
+        internal
+        onlyInitializing
+    {
         _trustedForwarder = trustedForwarder;
     }
 
-    function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
+    function isTrustedForwarder(address forwarder)
+        public
+        view
+        virtual
+        returns (bool)
+    {
         return forwarder == _trustedForwarder;
     }
 
-    function _msgSender() internal view virtual override returns (address sender) {
+    function _msgSender()
+        internal
+        view
+        virtual
+        override
+        returns (address sender)
+    {
         if (isTrustedForwarder(msg.sender)) {
             // The assembly code is more direct than the Solidity version using `abi.decode`.
             /// @solidity memory-safe-assembly
@@ -34,7 +52,13 @@ abstract contract ERC2771ContextUpgradeable is Initializable, ContextUpgradeable
         }
     }
 
-    function _msgData() internal view virtual override returns (bytes calldata) {
+    function _msgData()
+        internal
+        view
+        virtual
+        override
+        returns (bytes calldata)
+    {
         if (isTrustedForwarder(msg.sender)) {
             return msg.data[:msg.data.length - 20];
         } else {
