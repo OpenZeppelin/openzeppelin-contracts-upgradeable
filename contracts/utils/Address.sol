de60a1ad43213dc6588806bbc3d107563c19ca9f
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-18 22:40:41 +0100

    update to match vanilla master

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -178,11 +178,8 @@ library Address {
      * _Available since v3.4._
      */
     function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
-        require(isContract(target), "Address: delegate call to non-contract");
-
-        // solhint-disable-next-line avoid-low-level-calls
         (bool success, bytes memory returndata) = target.delegatecall(data);
-        return _verifyCallResult(success, returndata, errorMessage);
+        return verifyCallResultFromTarget(target, success, returndata, errorMessage);
     }
 
     /**
